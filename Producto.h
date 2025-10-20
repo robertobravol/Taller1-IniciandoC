4f2cad29c25f1856df0ca2ea4b1520bad560d1e0
robertobravol <robertobravolpz@gmail.com>
2025-10-20 10:07:04 -0500

    Se crea el archivo .h

---
+++ Producto.h
@@ -0,0 +1,22 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+#include <stdint.h>
+
+#define PRODUCTO_NOMBRE_MAX 128
+
+// Estructura para producto
+typedef struct Producto {
+	char nombre[PRODUCTO_NOMBRE_MAX]; // Nombre del producto
+	int codigo;                       // Código identificador del producto
+	float precio;                     // Precio unitario
+	int stock;                        // Cantidad disponible en inventario
+} Producto;
+
+void agregarProducto(struct Producto producto); // Agrega un producto al inventario
+void verificarProducto(struct Producto producto); // Verifica la existencia de un producto
+void ingresarProducto(); // Permite ingresar un nuevo producto
+void mostrarProducto(struct Producto producto); // Muestra la información de un producto
+
+#endif
+
